8815c5b6f5e170cc6037541af47771f151caf4df
Ricccc2k12 <ric.gaming2k12@gmail.com>
2025-10-09 22:10:36 +0200

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,24 @@
+field1 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field2 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field3 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field4 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field5 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field6 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field7 = [" ", " ", " ", " ", " ", " ", " ", " "]
+field8 = [" ", " ", " ", " ", " ", " ", " ", " "]
+
+def playerExistance():
+    field1[0] = "1"
+
+def fieldPrint():
+    print(field1)
+    print(field2)
+    print(field3)
+    print(field4)
+    print(field5)
+    print(field6)
+    print(field7)
+    print(field8)
+
+playerExistance()
+fieldPrint()
